b2a573f187850d4561fd4c6f8256fe4ed02365c4
sriya-g <sriya.gottiparthi@gmail.com>
2025-12-29 14:09:30 -0600

    added extra paths

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/dev-ada-website",  // add this
+  basePath: "/dev-ada-website",
+  assetPrefix: "/dev-ada-website",
   images: { unoptimized: true },
 };
 
